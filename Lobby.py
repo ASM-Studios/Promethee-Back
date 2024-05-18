2603e395fe51984b5eb7d68e8b5feb1ec1a56663
Djangss <mathieu.coulet@epitech.eu>
2024-05-18 14:42:08 +0200

    feat(routes): enter_lobby create and join lobby

--- Lobby.py
+++ Lobby.py
@@ -1,27 +1,35 @@
 import uuid as uuidGen
+import sys
 
 from Player import Player
 
 
 class Lobby:
     def __init__(self, uuid, maxUser=8):
+        self.__creator = None
         self.__uuid = uuid
         self.__players = []
         self.__maxUser = maxUser
 
     def addUser(self, player: Player):
-        if (self.isFull):
+        if self.isFull():
             raise Exception("Lobby is full")
         self.__players.append(player)
 
     def isFull(self):
-        if len(self.__players) == self.__maxUser:
+        if len(self.__players) >= self.__maxUser:
             return True
         else:
             return False
 
     def getUUID(self):
         return self.__uuid
+
+    def getCreator(self):
+        return self.__creator
+
+    def addCreator(self, creator):
+        self.__creator = creator
 
     def getPlayers(self):
         return self.__players
@@ -32,12 +40,9 @@ class LobbyManager:
         self.__lobby = []
 
     def createLobby(self, uuid):
-        for i in self.__lobby:
-            if (uuid == i.getUUID()):
-                uuid = uuidGen.uuid4().__str__()[0:6]
         newLobby = Lobby(uuid)
         self.__lobby.append(newLobby)
-        print(uuid)
+        return newLobby
 
     def get_lobbies(self):
         return self.__lobby
